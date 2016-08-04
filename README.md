db3f833a7dc4f2ab8e079599804d9481a6a00b43
Kumar-B <kumar.biswaranjan@yahoo.com>
2016-08-04 21:19:48 +0530

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# one-click-feeds-firefox
+# ![One click feeds icon](data/images/One-click-feeds-64X64.png)  one-click-feeds-firefox
 Firefox extension to open feedly feeds as tabs in one click.
+
+# Features
+  * Blacklisted URLs option
+  * Maximum number of feeds to open option
+# Usage
+Navigate to extension page in firefox `about:addons`. Go to `Extensions --> One Click Feeds --> Options`
+
+![One click feeds options](data/images/Options.png)
+
+## Blacklisted URLs
+It takes a comma separated urls as input. Any feed url that starts with any of these blacklisted urls will not open any tab and it will be automatically marked as read in feedly account so that it won't appear in future feedly page.
+
+For Example in the above image, there are two urls given.
+  * http://www.thehindu.com/business/
+  : This options will mark all the bussiness news from the hindu as read and no tabs will be opened.
+  * http://www.fairobserver.com/region/latin_america/
+  : This option will automatically mark all latin_america region news from fairobserver.
+
+## Maximum number of feeds to open
+This options enables to configure a max number of tabs to open at a time. For Example in the above option, maximum 10 tabs will be opened at a time.
+
+
+# Installation
+## Prerequisite
+* nodejs
+* jpm `npm install -g jpm`
+
+## Install signed extension
+* Download latest xpi file. Open the xpi file using Firefox.
+
+## Developement
+  * `git clone https://github.com/kumar-b/one-click-feeds-firefox.git`
+  * Run `jpm xpi`
+  * Open `one-click-feeds.xpi` in Firefox
+  * You may need to sign it using jpm to run in release version of firefox.
+
+# Contribution
+
+## Extended from
+firefox-feedly-tabs by splattael
+https://github.com/splattael/firefox-feedly-tabs
+
+## Logo
+Conceptualized and Designed by **Umashankar**
+[umashankar.ng@gmail.com](mailto:umashankar.ng@gmail.com)
+
+## License
+
+[MIT License](LICENSE.txt)
---
+++ data/one-click-feeds.js
@@ -0,0 +1,88 @@
+/**
+ * Iterates over each link found on a Feedly page emiting its url to +callback+.
+ */
+
+var blacklist_urls = "";
+var maxlinks = 0;
+
+// console.log(document.cookie);
+var feedly_data = extract_feedly_data(document.cookie);
+var token = feedly_data.feedlyToken || feedly_data.feedlyRefreshToken;
+// console.log("token", token);
+
+/*
+self.port.on("blaclist_pref_change", function(blacklist_urls_pref) {
+  console.log('blacklist changed to - ' + blacklist_urls_pref);
+  blacklist_urls = blacklist_urls_pref;
+  self.port.emit("config_loaded");
+});
+*/
+
+function eachFeedlyUrl(maxlinks, callback) {
+  var link_attribute = "data-alternate-link";
+  var links = document.querySelectorAll("div[" + link_attribute + "]");
+  var numLinksToOpen = maxlinks > 0 ? maxlinks : links.length;
+  for (var i = 0; i < numLinksToOpen; i++) {
+    var link = links[i];
+    var entryid = link.getAttribute("data-entryid");
+    var url = link.getAttribute(link_attribute);
+    //if (!url.match(/(^http:\/\/www\.thehindu\.com\/news\/national\/(andhra-pradesh|kerala|tamil-nadu|karnataka|telangana)\/.*)/g)) {
+      callback(entryid, url);
+    //} else {
+      //continue;
+    //}
+  }
+}
+
+function isBlackListed(url) {
+  var blacklisted_urls = blacklist_urls ? blacklist_urls.split(',') : [];
+  var urlIndex = blacklisted_urls.length;
+  while (urlIndex--) {
+    var bl_url = blacklisted_urls[urlIndex];
+    if (url.startsWith(bl_url)) {
+      console.info(url + ' matches blacklisted pattern- ' + bl_url);
+      return true;
+    }
+  }
+  return false;
+}
+
+self.port.on("process_feedly", function(preference) {
+  console.info('processing feedly links');
+  blacklist_urls = preference.blacklists;
+  maxlinks = preference.maxlinks;
+  console.info('blacklist option- ' + blacklist_urls);
+  console.info('maxlinks option- ' + maxlinks);
+
+  eachFeedlyUrl(maxlinks, function(entryid, url) {
+    if (isBlackListed(url)) {
+      var data = { token: token, link: {url: url, entryid: entryid} };
+      self.port.emit("mark_as_read", data);
+    } else {
+      self.port.emit("link", { entryid: entryid, url: url });
+    }
+    /*if (!url.match(/(^http:\/\/www\.thehindu\.com\/news\/national\/(andhra-pradesh|kerala|tamil-nadu|karnataka|telangana|other-states)\/.*)/g)) {
+     self.port.emit("link", { entryid: entryid, url: url });
+     } else {
+     var data = { token: token, link: {url: url, entryid: entryid} };
+     self.port.emit("mark_as_read", data);
+     }*/
+  });
+});
+
+function extract_feedly_data(cookie) {
+  var regexp = /feedly\.session=(\{.*?\})/;
+  var matched = cookie.match(regexp);
+  // console.log(matched[1]);
+  if (matched) {
+    return JSON.parse(matched[1]);
+  }
+}
+
+
+self.port.on("tab_closed", function(link) {
+  // console.log("tab_closed", token, link);
+  var data = { token: token, link: link };
+  // console.log("data", data);
+  self.port.emit("mark_as_read", data);
+});
